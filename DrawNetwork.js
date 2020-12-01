ef559165171b95ec43823050880ff7444efc87c2
Anamariabd <anabuenahoradiaz@gmail.com>
2020-11-30 21:49:37 -0500

    agregando el grafo al archivo script

--- DrawNetwork.js
+++ DrawNetwork.js
@@ -34,7 +34,7 @@ var network = new vis.Network(container, data, options);
 // Esto se repite cada 2 segundos
 setInterval(()=>{
   if(isCounting){
-    // cuando se empieza a hacer el movimiento de la cinta se hace esto:
+    // cuando se empieza a hacer el movimiento de la cinta se hace esto en el grafo:
 var dot1 = 'dinetwork {node[shape=circle]; 1 -> 2 [label="B, B, L"]; 2 -> 3[label="B, B, R"]; 1 -> 1[label="b, a, R|a, a, R"]; 2 -> 2[label="a, a, L"]; 3[ borderWidth=7];';
 
 var data = vis.parseDOTNetwork(dot);
@@ -42,12 +42,17 @@ if(isCounting){
   console.log("hola si entro yo");
 }
 setTimeout(() =>{ 
-  dot = dot1+' 1[color=green]; 1->1[color = blue];  1->2[color = blue]}';
-  var network = new vis.Network(container, data, options)}, 1000);
+  dot = dot1+' 1[color=green]; 1->1[color = green];  1->2[color = blue]}';
+  var network = new vis.Network(container, vis.parseDOTNetwork(dot), options)}, 2000);
 
 setTimeout(() =>{ 
   dot = dot1+'2[color=green]; 2->2[color = blue]; 2->3[color = blue]}'; 
   var network = new vis.Network(container, vis.parseDOTNetwork(dot), options)}, 2000);
+
+
+setTimeout(() =>{ 
+    dot = dot1+'2[color=green]; 2->2[color = green]; 2->3[color = blue]}'; 
+    var network = new vis.Network(container, vis.parseDOTNetwork(dot), options)}, 1000);
 
 setTimeout(() =>{ 
   dot = dot1+'3[color=green]}'; 
--- script.js
+++ script.js
@@ -5,7 +5,39 @@ let isCounting = false
 let line = []
 let caretpos = 0 // posicion carrete (creo)
 let initx = true
+var container = document.getElementById("mynetwork");
+var dot = 'dinetwork {node[shape=circle]; 1 -> 2 [label="B, B, L"]; 2 -> 3[label="B, B, R"]; 1 -> 1[label="b, a, R|a, a, R"]; 2 -> 2[label="a, a, L"]; 3[ borderWidth=7]}';
+var data = vis.parseDOTNetwork(dot);
+var dot1 = " ";
 
+var options = {
+    nodes: {
+      shape: "dot",
+      size: 50,
+      font: {
+        size: 32,
+      },
+      borderWidth: 1,
+      shadow: true,
+    },
+    layout: {
+      hierarchical: {
+        sortMethod: "directed",
+      },
+    },
+    edges: {
+      width: 3,
+      length: 190,
+      font: {
+        size: 16,
+      },
+      shadow: true,
+      color: "blue",
+    },
+  };
+
+  
+var network = new vis.Network(container, data, options);
 
 begin = () => {
     init()
@@ -41,11 +73,10 @@ init = () => {
         alert('no valida')
         return
     }
-    line = str
-
+    line = " "+str+" ";
     
     const parent = document.querySelector('.digit-list')
-    for (let i = 0; i < str.length; i++) {
+    for (let i = 0; i < str.length+2; i++) {
         let child = document.createElement("div")
         child.classList.add("digit")
         parent.appendChild(child)
@@ -82,6 +113,8 @@ reload = () => {
     if (cell.hasChildNodes()) {
         while (cell.childNodes.length >= 1) {
             cell.removeChild(cell.firstChild);
+         dot = 'dinetwork {node[shape=circle]; 1 -> 2 [label="B, B, L"]; 2 -> 3[label="B, B, R"]; 1 -> 1[label="b, a, R|a, a, R"]; 2 -> 2[label="a, a, L"]; 3[ borderWidth=7]}';
+        var network = new vis.Network(container, data, options);
         }
     }
 
@@ -99,27 +132,40 @@ setStatus = (status, delay = 1200) => {
 
 
 start = () => {
+    
+    dot1 = 'dinetwork {node[shape=circle]; 1 -> 2 [label="B, B, L"]; 2 -> 3[label="B, B, R"];  2 -> 2[label="a, a, L"]; 3[ borderWidth=7];';
+
+    var data = vis.parseDOTNetwork(dot);
     isCounting = true
+    
+    x = 0;
     document.querySelectorAll('.digit').forEach((block, inx) => {
-        
         window.setTimeout(() => {
             document.querySelector('.status').innerHTML = 'moviendo la cinta...'
             setStatus('leyendo cinta...', 1100)
-
-            caretpos = inx
+            caretpos = inx;
             console.log('b'.includes(line[inx]));
             setCaret()
-            
             if ('b'.includes(line[inx])) {
+        // Pintando estado 1      
+                window.setTimeout(() =>{ 
+                dot = dot1+' 1 -> 1[label="b, a, R"]; 1[color=green]; 1->1[color = green];  1->2[color = blue]}';
+                var network = new vis.Network(container, vis.parseDOTNetwork(dot), options)}, 1800);
+         //Sobreescribiendo
                 setStatus('sobreescribiendo a ', 1800)
                 line[inx] = 'a'
                 window.setTimeout(() => {
                     block.innerHTML = 'a'
                 }, 1800)
             } else {
+
+                window.setTimeout(() =>{ 
+                dot = dot1+' 1 -> 1[label="a, a, R"]; 1[color=green]; 1->1[color = green];  1->2[color = blue]}';
+                var network = new vis.Network(container, vis.parseDOTNetwork(dot), options)}, 1800);
                 setStatus('', 1800)
             }
         }, 2500 * inx)
+        x++;
     })
 
     window.setTimeout(() => {
